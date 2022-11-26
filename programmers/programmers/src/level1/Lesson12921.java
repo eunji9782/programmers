f9f8fc1df922d975a5319bd3db0f4e84f71563cb
DESKTOP-P9TJN6R\gldi9 <dmswl9782@gmail.com>
2022-11-27 00:03:03 +0900

    11/26 1문제

---
+++ programmers/programmers/src/level1/Lesson12921.java
@@ -0,0 +1,46 @@
+package level1;
+
+public class Lesson12921 {
+
+	public static void main(String[] args) {
+
+		int answer = new PrimeNumber().solution(1000000);
+		System.out.println(answer);
+	}
+
+}
+
+class PrimeNumber {
+    public int solution(int n) {
+		/* 시간초과
+		 * int answer = 0;
+		 * 
+		 * for(int i = 2; i <= n; i++) { boolean checkPrime = false;
+		 * 
+		 * if(i % 2 == 0 && i != 2) { continue; }
+		 * 
+		 * 
+		 * for(int j = 2; j < i-1; j++) { if(i % j == 0) { checkPrime = true; continue;
+		 * } }
+		 * 
+		 * if(!checkPrime) { answer++; } }
+		 */
+    	//에라토스테네스의 체를 이용한 풀이 : n까지 돌리는게 아닌 n의 제곱근번 돌려도 판별이 가능하다
+    	int answer = 0;
+    	
+    	int[] arr = new int[n + 1];
+
+        for (int i = 2; i <= (int) Math.sqrt(n); ++i) {
+            if (arr[i] == 1) continue;
+            for (int j = i + i; j <= n; j += i) {
+                arr[j] = 1;
+            }
+        }
+        
+        for (int i = 2; i < arr.length; ++i) {
+            if (arr[i] != 1) ++answer;
+        }
+
+        return answer;
+    }
+}
